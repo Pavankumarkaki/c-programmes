dbc8ad8b703bc8a970bb700ac101150afb330179
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-01-17 16:39:06 +0530

    Add files via upload

---
+++ LCM1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b,i,c,x,y,count=0;
+	printf("enter the number for LCM");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		x=a;
+		y=b;	
+	}
+	else
+	{
+		x=b;
+		y=a;
+	}
+	for(i=1;i<=y;i++)
+	{
+		c=x*i;
+		//printf("%d ",c);
+		if(c%y==0)
+		{
+			printf("\nLCM is : %d ",c);
+			break;
+		} 
+		
+	}
+}
