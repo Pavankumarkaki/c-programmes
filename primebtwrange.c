aac87228280c83509de440041fbb0847ee53540f
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-01-19 10:31:51 +0530

    Add files via upload

---
+++ primebtwrange.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i,countmain=0,countsub=0,j;
+    scanf("%d%d",&m,&n);//2,13
+    for(i=m;i<=n;i++)//2<13
+    {
+       for(j=1;j<=i;j++)//1<2
+       {
+       		if(i%j==0)
+       		{
+       		  countsub++;
+       		  
+			}
+	   }
+	  if(countsub==2)
+	  {
+	  	//countmain++;
+	  	printf("%d ",i);
+	  }
+	   countsub=0;	
+	   
+    }
+    //printf("%d",countmain);
+}
